c0b58c1ef32d2a21c6928a5e373bcb9129420e66
Adam Malinowski <a.malinowsk2@partner.samsung.com>
2014-12-10 10:17:06 +0100

    Add UI backend based on tizen notifications
    Change-Id: Iec59517b8d5a55f54ca831394a12793c2ef2ae4c

--- src/agent/main/Agent.cpp
+++ src/agent/main/Agent.cpp
@@ -31,6 +31,8 @@
 #include <types/AgentErrorMsg.h>
 #include <types/SupportedTypes.h>
 
+#include <ui/AskUINotificationBackend.h>
+
 #include "Agent.h"
 
 namespace AskUser {
@@ -181,7 +183,7 @@ void Agent::processUIResponse(const Response &response) {
 
 bool Agent::startUIForRequest(RequestPtr request) {
     auto data = Translator::Agent::dataToRequest(request->data());
-    auto ui = std::shared_ptr<AskUIInterface>(); // TODO: create pointer to backend
+    auto ui = std::make_shared<AskUINotificationBackend>();
 
     auto handler = [&](RequestId requestId, UIResponseType resultType) -> void {
                        UIResponseHandler(requestId, resultType);
@@ -190,6 +192,7 @@ bool Agent::startUIForRequest(RequestPtr request) {
     if (ret) {
         m_UIs[request->id()] = ui;
     }
+
     return ret;
 }
 
