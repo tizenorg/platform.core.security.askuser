9d134af2d1b5cdb68aeb51c88636309f48e8e7fa
Oskar Świtalski <o.switalski@samsung.com>
2016-03-23 11:51:37 +0100

    Rework main daemon
    Signed-off-by: Oskar Świtalski <o.switalski@samsung.com>

--- src/daemon/NotificationTalker.cpp
+++ src/daemon/NotificationTalker.cpp
@@ -1,62 +1,290 @@
+/*
+ *  Copyright (c) 2016 Samsung Electronics Co.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/**
+ * @file        NotificationTalker.cpp
+ * @author      Oskar Świtalski <o.switalski@samsung.com>
+ * @brief       Definition of NotificationTalker class
+ */
+
 #include "NotificationTalker.h"
 
+#include <algorithm>
+#include <cstring>
 #include <cynara-creds-socket.h>
-#include "../common/notification-ipc.h"
-#include <iostream>
+#include <sys/stat.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <common/Exception.h>
+#include <common/log.h>
+#include <common/Types.h>
+#include <common/Translator.h>
 
 NotificationTalker::NotificationTalker()
 {
+  int ret, len;
+  sockaddr_un local;
+
+  m_sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+  if (m_sockfd == -1)
+    throw Exception("Creating socket error", errno);
+
+  local.sun_family = AF_UNIX;
+  strcpy(local.sun_path, socketpath);
+  unlink(socketpath);
+  len = strlen(local.sun_path) + sizeof(local.sun_family);
+
+  ret = bind(m_sockfd, (struct sockaddr *)&local, len);
+  if (ret == -1)
+    throw Exception("Binding socket error", errno);
+
+  ret = listen(m_sockfd, 10);
+  if (ret == -1)
+    throw Exception("Listening socket error", errno);
 }
 
-void NotificationTalker::add(int fd)
+void NotificationTalker::parseRequest(CynaraRequestPtr request)
 {
-  char *user_c = nullptr;
-  cynara_creds_socket_get_user(fd, USER_METHOD_DEFAULT,&user_c);
-  std::string user = user_c ? user_c : "0";
-  fdMap[user] = fd;
-  free(user_c);
-  //FD_SET(fd ,&fdSet);
+  switch (request->type) {
+  case RequestType::RT_Close:
+    LOGD("Close service");
+    stop();
+    return;
+  case RequestType::RT_Action:
+    LOGD("Add request: " << request->id);
+    addRequest(request);
+    return;
+  case RequestType::RT_Cancel:
+    LOGD("Cancel request: " << request->id);
+    removeRequest(request);
+    return;
+  default:
+    return;
+  }
+}
 
-  for (int i = 0; true; ++i) {
+void NotificationTalker::addRequest(CynaraRequestPtr request)
+{
+  std::lock_guard<std::mutex> lock(m_mutex);
 
-    GuiResponse resp;
-    Recieve buf;
-    int buf_siz = buf.set("app ka "+ std::to_string(i), "perma: user" + user);
-    send(fd, &buf, buf_siz, 0);
+  auto &queue = requests[request->user];
+  auto it = std::find_if(
+        queue.begin(), queue.end(), [&request](CynaraRequestPtr req){return req->id == request->id;}
+      );
 
-    recv(fd, &resp, sizeof(resp), 0);
+  if (it == queue.end()) {
+    queue.push_back(request);
+  } else {
+    LOGD("Cynara request already exists");
+  }
 
-    switch (resp)
-    {
-    case GuiResponse::Allow:
-      std::cout << "allow" << std::endl;
-      break;
-    case GuiResponse::Deny:
-      std::cout << "deny" << std::endl;
-      break;
-    case GuiResponse::Never:
-      std::cout << "never" << std::endl;
-      break;
-    default:
-      std::cout << "error" << std::endl;
-      break;
+}
+
+void NotificationTalker::removeRequest(CynaraRequestPtr request)
+{
+  std::lock_guard<std::mutex> lock(m_mutex);
+
+  for (auto &pair : requests) {
+    auto &queue = std::get<1>(pair);
+    auto it = std::find_if(
+          queue.begin(), queue.end(), [&request](CynaraRequestPtr req){return req->id == request->id;}
+        );
+
+    if (it != queue.end()) {
+      queue.erase(it);
     }
   }
 }
 
-void NotificationTalker::start()
+void NotificationTalker::setResponseHandler(ResponseHandler responseHandler)
 {
-  m_thread = std::thread(&NotificationTalker::run, this);
+  m_responseHandler = responseHandler;
 }
 
-void NotificationTalker::addReq(CynaraRequest q)
+void NotificationTalker::stop()
 {
+  m_stopflag = true;
+  for (auto& pair : m_fdStatus) {
+    close(std::get<0>(pair));
+  }
+  close(m_sockfd);
+}
 
+void NotificationTalker::sendRequest(int fd, const CynaraRequestPtr request)
+{
+  int size;
+  int len;
+  int ret;
+
+  m_fdStatus[fd] = false;
+
+  std::string data = recieveToData(request->id, request->app, request->perm);
+  size = data.size();
+
+  len = send(fd, &size, sizeof(size), 0);
+  if (len <= 0)
+    throw Exception("Error sending data to socket", errno);
+
+  len = send(fd, data.c_str(), size, 0);
+  if (len <= 0)
+    throw Exception("Error sending data to socket", errno);
+}
+
+void NotificationTalker::parseResponse(Response response, int fd)
+{
+  CynaraRequestPtr request = nullptr;
+
+  /* lock_guard */ {
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    auto &queue = requests[m_fdToUser[fd]];
+    if (queue.empty()) {
+      LOGD("Request canceled");
+      m_fdStatus[fd] = true;
+      return;
+    }
+
+    request = queue.front();
+    if (request->id != response.id) {
+      LOGD("Request canceled");
+      m_fdStatus[fd] = true;
+      return;
+    }
+
+    queue.pop_front();
+  } /* lock_guard */
+
+  LOGD("For user: <" << request->user << "> client: <" << request->app << "> permision: <" <<
+       request->perm << "> recieved: <" << GuiResponseToString(response.response) << ">");
+  m_responseHandler(response);
+
+  uint8_t ack = 0xAC;
+  send(fd, &ack, sizeof(ack), 0);
+
+  m_fdStatus[fd] = true;
+}
+
+void NotificationTalker::recvResponses()
+{
+  for (auto pair : m_userToFd) {
+    int fd = std::get<1>(pair);
+
+    if (FD_ISSET(fd, &m_fdSet)) {
+      Response response;
+
+      int len = recv(fd, &response, sizeof(response), 0);
+      if (len < 0) {
+        throw Exception("Error reciving data from socket", errno);
+      } else if (len) {
+        parseResponse(response, fd);
+      } else {
+        remove(fd);
+      }
+    }
+  }
+}
+
+void NotificationTalker::newConnection()
+{
+  if (FD_ISSET(m_sockfd, &m_fdSet)) {
+    int fd;
+    sockaddr_un remote;
+    socklen_t t = sizeof(remote);
+
+    fd = accept(m_sockfd, (sockaddr*)&remote, &t);
+    if (fd < 0)
+      throw Exception("Accepting socket error", errno);
+
+    char *user_c = nullptr;
+
+    cynara_creds_socket_get_user(fd, USER_METHOD_DEFAULT,&user_c);
+    std::string user = user_c ? user_c : "0";
+
+    auto it = m_userToFd.find(user);
+    if (it != m_userToFd.end())
+      remove(std::get<1>(*it));
+
+    m_userToFd[user] = fd;
+    m_fdToUser[fd] = user;
+    m_fdStatus[fd] = true;
+
+    free(user_c);
+
+    LOGD("Accepted new conection for user: " << user);
+  }
+}
+
+void NotificationTalker::remove(int fd)
+{
+  LOGE("Close sock " << fd);
+  close(fd);
+  auto user = m_fdToUser[fd];
+  m_fdToUser.erase(fd);
+  m_userToFd.erase(user);
+  m_fdStatus.erase(fd);
 }
 
 void NotificationTalker::run()
 {
-  while(m_stopflag) {
+  LOGD("Notification loop started");
+  while (!m_stopflag) {
 
+    timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 1000*100;
+
+    int nfds = m_sockfd;
+
+    FD_ZERO(&m_fdSet);
+    FD_SET(m_sockfd, &m_fdSet);
+
+    for (auto pair : m_userToFd) {
+      int fd = std::get<1>(pair);
+      FD_SET(fd ,&m_fdSet);
+      nfds = fd > nfds ? fd : nfds;
+    }
+    nfds++;
+
+    int rv = select(nfds + 1, &m_fdSet, nullptr, nullptr, &timeout);
+
+    if (m_stopflag)
+      break;
+
+    if (rv < 0) {
+      throw Exception("error on select", errno);
+    } else if (rv) {
+      recvResponses();
+      newConnection();
+    } else {
+      // timeout
+    }
+
+    /* lock_guard */ {
+      std::lock_guard<std::mutex> lock(m_mutex);
+      for (auto pair : m_fdStatus ) {
+        int fd = std::get<0>(pair);
+        bool b = std::get<1>(pair);
+        auto &queue = requests[m_fdToUser[fd]];
+        if(b && !queue.empty()) {
+          CynaraRequestPtr request = queue.front();
+          sendRequest(fd, request);
+        }
+      }
+    } /* lock_guard */
   }
+
+  LOGD("NotificationTalker loop ended");
 }
--- src/daemon/NotificationTalker.h
+++ src/daemon/NotificationTalker.h
@@ -1,52 +1,79 @@
-#pragma once
+/*
+ *  Copyright (c) 2016 Samsung Electronics Co.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/**
+ * @file        NotificationTalker.cpp
+ * @author      Oskar Świtalski <o.switalski@samsung.com>
+ * @brief       Declaration of NotificationTalker class
+ */
 
+#ifndef __NOTIFICATION_TALKER__
+#define __NOTIFICATION_TALKER__
+
+#include <deque>
+#include <functional>
 #include <map>
 #include <mutex>
-#include <queue>
 #include <string>
 #include <sys/socket.h>
-#include <thread>
 
-#include <cynara-agent.h>
-#include <cynara-plugin.h>
+#include <common/Types.h>
 
-typedef enum {
-    RT_Action,
-    RT_Cancel,
-    RT_Close
-} RequestType;
+typedef std::pair<std::string, int> UserToFdPair;
+typedef std::map<std::string, int> UserToFdMap;
+typedef std::map<int, std::string> FdToUserMap;
+typedef std::map<int, bool> FdStatus;
 
-typedef cynara_agent_req_id RequestId;
+typedef std::map<std::string, std::deque<CynaraRequestPtr>> RequestsQueue;
 
-class CynaraRequest
-{
-public:
-  RequestId m_id;
-  RequestType m_type;
-  Cynara::PluginData m_data;
-};
+typedef std::function<void(Response)> ResponseHandler;
 
 class NotificationTalker
 {
 public:
   NotificationTalker();
-  void add(int fd);
-  void start();
 
-  void addReq(CynaraRequest q);
+  void parseRequest(CynaraRequestPtr request);
+  void run();
+  void setResponseHandler(ResponseHandler responseHandler);
+  void stop();
 
 private:
-  std::map<std::string, int> fdMap;
-  std::map<int, std::queue<CynaraRequest>> queues;
-  std::map<RequestId, CynaraRequest> responseQueue;
+  ResponseHandler m_responseHandler;
 
-  std::mutex mutex;
+  UserToFdMap m_userToFd;
+  FdToUserMap m_fdToUser;
+  FdStatus m_fdStatus;
+  fd_set m_fdSet;
+  int m_sockfd;
 
-  std::thread m_thread;
+  RequestsQueue requests;
+  std::mutex m_mutex;
+
   bool m_stopflag = false;
-  fd_set fdSet;
 
-  void run();
+  void parseResponse(Response response, int fd);
+  void recvResponses();
 
+  void newConnection();
+  void remove(int fd);
+
+  void addRequest(CynaraRequestPtr request);
+  void removeRequest(CynaraRequestPtr request);
+  void sendRequest(int fd, const CynaraRequestPtr request);
 };
+
+#endif /* __NOTIFICATION_TALKER__ */
 
