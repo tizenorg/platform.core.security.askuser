170f1a2ef4ca2abd5a9b3bd54e0dba6c8c2c892c
Oskar Świtalski <o.switalski@samsung.com>
2016-05-31 16:36:52 +0200

    Add socket path description
    Change-Id: I91769189f3034b10c1f9b5fe9725153f34c49013

---
+++ src/common/config/Path.h
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2016 Samsung Electronics Co.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/**
+ * @file        src/common/config/Path.h
+ * @author      Zofia Abramowska <z.abramowska@samsung.com>
+ * @brief       Definition of common types and consts
+ */
+
+#pragma once
+
+#include <string>
+
+namespace AskUser {
+namespace Path {
+
+const std::string &getSocketPath();
+
+} // namespace Path
+} // namespace AskUser
