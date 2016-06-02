de5c7746151d4df1adf8c505a675df427f121347
Zofia Abramowska <z.abramowska@samsung.com>
2016-06-02 12:42:07 +0200

    Add notification request and response
    Change-Id: I3130590540d118fec365176b95df2717a84e266a

---
+++ src/common/types/NotificationResponse.h
@@ -0,0 +1,44 @@
+
+/*
+ *  Copyright (c) 2016 Samsung Electronics Co.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/**
+ * @file        src/common/types/NotificationResponse.h
+ * @author      Oskar Świtalski <o.switalski@samsung.com>
+ * @brief       Definition of common types and consts
+ */
+
+#pragma once
+
+#include <cynara-agent.h>
+#include <types/RequestId.h>
+
+namespace AskUser {
+
+enum class NResponseType
+{
+    Allow,
+    Deny,
+    Never,
+    Error,
+    None
+};
+
+struct NotificationResponse {
+    RequestId id;
+    NResponseType response;
+};
+
+} // namespace AskUser
