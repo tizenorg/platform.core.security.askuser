Samsung/askuser

Use syslog when journal is not available
This patch allows compilation of askuser on distributions without journal.

On distributions without systemd and journald
both of these components will be detected by cmake.
If journalctl is available, it will be used.
Otherwise syslog will be used.

Change-Id: I545118716bfd813cab86d243358cc94b777ca9ce
Signed-off-by: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>

src/agent/log/alog.h
@@ -18,6 +18,7 @@
 /**
  * @file        alog.h
  * @author      Adam Malinowski <a.malinowsk2@partner.samsung.com>
+ * @author      Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
  * @version     1.0
  * @brief       This file defines agent logging utilities.
  */
@@ -25,16 +26,43 @@
 #pragma once
 
 #include <sstream>
+
+#ifdef BUILD_WITH_SYSTEMD
 #include <systemd/sd-journal.h>
+#else // BUILD_WITH_SYSTEMD
+#include <syslog.h>
+#endif // BUILD_WITH_SYSTEMD
 
 extern int __alog_level;
+
+#define UNUSED __attribute__((unused))
+
+namespace {
+    template <typename ...Args>
+    void UNUSED __ALOG_FUN(int level, const std::stringstream &format, Args&&... args) {
+#ifdef BUILD_WITH_SYSTEMD
+        sd_journal_print(level, format.str().c_str(), std::forward<Args>(args)...);
+#else // BUILD_WITH_SYSTEMD
+        syslog(level, format.str().c_str(), std::forward<Args>(args)...);
+#endif // BUILD_WITH_SYSTEMD
+    }
+
+    template <>
+    void UNUSED __ALOG_FUN(int level, const std::stringstream &format) {
+#ifdef BUILD_WITH_SYSTEMD
+        sd_journal_print(level, "%s", format.str().c_str());
+#else // BUILD_WITH_SYSTEMD
+        syslog(level, "%s", format.str().c_str());
+#endif // BUILD_WITH_SYSTEMD
+    }
+} // namespace anonymous
 
 #define __ALOG(LEVEL, FORMAT, ...) \
     do { \
         if (LEVEL <= __alog_level) { \
-            std::stringstream __LOG_MACRO_format; \
-            __LOG_MACRO_format << FORMAT; \
-            sd_journal_print(LEVEL, __LOG_MACRO_format.str().c_str(), ##__VA_ARGS__); \
+            std::stringstream __ALOG_MACRO_format; \
+            __ALOG_MACRO_format << FORMAT; \
+            __ALOG_FUN(LEVEL, __ALOG_MACRO_format, ##__VA_ARGS__); \
         } \
     } while (0)
 

tizenorg/platform.core.security.askuser

src/common/log/alog.h
@@ -18,6 +18,7 @@
 /**
  * @file        alog.h
  * @author      Adam Malinowski <a.malinowsk2@partner.samsung.com>
+ * @author      Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
  * @version     1.0
  * @brief       This file defines agent logging utilities.
  */
@@ -25,16 +26,43 @@
 #pragma once
 
 #include <sstream>
+
+#ifdef BUILD_WITH_SYSTEMD_JOURNAL
 #include <systemd/sd-journal.h>
+#else // BUILD_WITH_SYSTEMD_JOURNAL
+#include <syslog.h>
+#endif // BUILD_WITH_SYSTEMD_JOURNAL
 
 extern int __alog_level;
+
+#define UNUSED __attribute__((unused))
+
+namespace {
+    template <typename ...Args>
+    void UNUSED __ALOG_FUN(int level, const std::stringstream &format, Args&&... args) {
+#ifdef BUILD_WITH_SYSTEMD_JOURNAL
+        sd_journal_print(level, format.str().c_str(), std::forward<Args>(args)...);
+#else // BUILD_WITH_SYSTEMD_JOURNAL
+        syslog(level, format.str().c_str(), std::forward<Args>(args)...);
+#endif // BUILD_WITH_SYSTEMD_JOURNAL
+    }
+
+    template <>
+    void UNUSED __ALOG_FUN(int level, const std::stringstream &format) {
+#ifdef BUILD_WITH_SYSTEMD_JOURNAL
+        sd_journal_print(level, "%s", format.str().c_str());
+#else // BUILD_WITH_SYSTEMD_JOURNAL
+        syslog(level, "%s", format.str().c_str());
+#endif // BUILD_WITH_SYSTEMD_JOURNAL
+    }
+} // namespace anonymous
 
 #define __ALOG(LEVEL, FORMAT, ...) \
     do { \
         if (LEVEL <= __alog_level) { \
-            std::stringstream __LOG_MACRO_format; \
-            __LOG_MACRO_format << FORMAT; \
-            sd_journal_print(LEVEL, __LOG_MACRO_format.str().c_str(), ##__VA_ARGS__); \
+            std::stringstream __ALOG_MACRO_format; \
+            __ALOG_MACRO_format << FORMAT; \
+            __ALOG_FUN(LEVEL, __ALOG_MACRO_format, ##__VA_ARGS__); \
         } \
     } while (0)
 